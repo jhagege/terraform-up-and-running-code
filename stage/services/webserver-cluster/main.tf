7b23482190396c4fe9633e8902595dabfe75d1e3
jhagege <cyberjoac@gmail.com>
2018-07-29 15:24:06 +0300

    Finished chapter 3

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -2,21 +2,31 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  default = 8080
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config {
+    bucket = "terraform-up-and-running-state-jo"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "template_file" "user_data" {
+  template = "${file("user-data.sh")}"
+
+  vars {
+    server_port = "${var.server_port}"
+    db_address  = "${data.terraform_remote_state.db.address}"
+    db_port     = "${data.terraform_remote_state.db.port}"
+  }
 }
 
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-40d28157"
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.instance.id}"]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
-              EOF
+  user_data       = "${data.template_file.user_data.rendered}"
 
   lifecycle {
     create_before_destroy = true
@@ -94,8 +104,4 @@ resource "aws_security_group" "elb" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-output "elb_dns_name" {
-  value = "${aws_elb.example.dns_name}"
 }
